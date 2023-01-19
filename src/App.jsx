f49bcf70e2ef14c6662a6887c70b5742aa9ce3cb
Ivan Paez Coronell <ipaezcoronell@outlook.com>
2023-01-19 11:40:18 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Task App</h1>
+    
+    </>
+     
+  )
+}
+
+export default App
